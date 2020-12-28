55c53681a95e1c2235d72695d9d4e2b763113b2d
清水園子 <s-shimizu@tsumikiinc.com>
2020-12-28 21:20:33 +0900

    動作確認用HOME

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,22 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-contentful-prev</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.home
+  .home__head
+    h1 テストprev
+  .home__body
+    |{{ posts }}
 </template>
-
 <script>
-export default {};
+import client from '~/plugins/contentful.js';
+export default {
+  async asyncData({ env, params }) {
+    const posts = await client.getEntries({
+      content_type: 'post',
+      order: '-fields.date',
+    });
+    return {
+      posts: posts.items,
+    };
+  },
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style lang="scss"></style>
