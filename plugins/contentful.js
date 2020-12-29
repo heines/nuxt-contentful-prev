ab0ef9955372a50b304c550af58ae9ab790329ad
清水園子 <s-shimizu@tsumikiinc.com>
2020-12-29 21:19:38 +0900

    コメント追加

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -4,12 +4,15 @@ let isPrev = false;
 if (process.env.CONTEXT === 'prev') {
   isPrev = true;
 }
+
+// def
 const config = {
   host: 'cdn.contentful.com',
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
 };
 
+// prev
 if (process.env.NODE_ENV !== 'production' || isPrev) {
   config.host = process.env.CTF_CPA_HOST;
   config.accessToken = process.env.CTF_CPA_ACCESS_TOKEN;
