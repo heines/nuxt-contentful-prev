f77ed5cb20c212c87b116dca7901c59e1c431bba
清水園子 <s-shimizu@tsumikiinc.com>
2020-12-29 21:11:06 +0900

    prevブランチではpreviewAPIを読む

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,11 +1,15 @@
 const contentful = require('contentful');
+let isPrev = false;
+if (process.env.CONTEXT === 'prev') {
+  isPrev = true;
+}
 const config = {
   host: 'cdn.contentful.com',
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN,
 };
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' || isPrev) {
   config.host = process.env.CTF_CPA_HOST;
   config.accessToken = process.env.CTF_CPA_ACCESS_TOKEN;
 }
